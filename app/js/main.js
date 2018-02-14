c0a54d300ed0e1d43e8384963b44feb39f349b51
YuryKoropenko <yurykoropenko@gmail.com>
2018-02-14 13:33:49 +0200

    full index-page

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,14 @@ $(function() {
 		nav:true,
 		items:1
 	});
+
+	$('.h-nav__btn').on('click', function() {
+		$(this).parent().children('.h-nav').slideToggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.h-nav').length) return;
+				$('.h-nav').slideUp();
+				event.stopPropagation();
+			});
+		return false;
+	});
 });
